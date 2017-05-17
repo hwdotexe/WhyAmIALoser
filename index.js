20a80243633b6672e8ba1b54fe935eca536f3da9
xFyreWolfx <hadengw@gmail.com>
2017-05-17 13:42:31 -0600

    Try to make this mobile-friendly
    Also added more reasons

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-// Site version 1.0
+// Site version 1.1
 // Created by xFyreWolfx - http://xfyrewolfx.tech/
 
-var reasons = ["You smell like green eggs and ham", "You're too school for cool", "Your grandma told you so", "Dank memes", "You wish your boyfriend was hot like I am", "You can't catch me outside", "Mayonnaise is not an instrument", "You're friends with the Nigerian Prince", "Deodorant: try it sometime", "I don't like your T-Shirt", "You're on this website", "Your height", "You've had one too many hot dogs. Or seven.", "You were grown in a lab... and you just found out", "You forgot the Alamo, didn't you?", "You're not a real boy", "You still can't find waldo", "You eat worms", "You're a comic book nerd", "Winning is illogical", "This site has deemed you one.", "You gotta get a job, hippie!", "You got your mobility back", "You're a carrot farmer", "You sunbathe on your roof. Naked. In the city.", "Science"];
-
+var reasons = ["You smell like green eggs and ham", "You're too school for cool", "Your grandma told you so", "Dank memes", "You wish your boyfriend was hot like I am", "You can't catch me outside", "Mayonnaise is not an instrument", "You're friends with the Nigerian Prince", "Deodorant: try it sometime", "I don't like your T-Shirt", "You're on this website", "Your height", "You've had one too many hot dogs. Or seven.", "You were grown in a lab... and you just found out", "You forgot the Alamo, didn't you?", "You're not a real boy", "You still can't find waldo", "You eat worms", "You're a comic book nerd", "Winning is illogical", "This site has deemed you one.", "You gotta get a job, hippie!", "You got your mobility back", "You're a carrot farmer", "You sunbathe on your roof. Naked. In the city.", "Science", "You've had your blinker on for the past 5 blocks", "You're waaay too old", "Victory is not yours", "We are not the champions", "Your losing streak is a world record"];
 var randomNumber, randomReason;
 var i = 0;
 
-while(i<reasons.length) {
-    i++;
-    var duplicate = false;
-    randomNumber = getRandom(0, reasons.length);
-    randomReason = reasons[randomNumber].toUpperCase();
+document.getElementsByTagName("body")[0].addEventListener("click", newReason);
 
-    if(i<reasons.length) {
-        getPastReasons().forEach(function (item) {
-            if (randomReason === item) {
-                duplicate = true;
-            }
-        });
+function newReason(){
+    while(i<reasons.length) {
+        i++;
+        var duplicate = false;
+        randomNumber = getRandom(0, reasons.length);
+        randomReason = reasons[randomNumber].toUpperCase();
+
+        if(i<reasons.length) {
+            getPastReasons().forEach(function (item) {
+                if (randomReason === item) {
+                    duplicate = true;
+                }
+            });
+        }
+
+        if(!duplicate){
+            break;
+        }
     }
 
-    if(!duplicate){
-        break;
-    }
+    document.getElementById("reason").textContent= randomReason;
+
+    if(i<reasons.length) storePastReasons();
+    i = 0;
 }
-
-document.getElementById("reason").textContent= randomReason;
-
-if(i<reasons.length) storePastReasons();
 
 function storePastReasons(){
     var pr = getPastReasons();
@@ -42,3 +46,5 @@ function getPastReasons(){
 function getRandom(bottom, top) {
     return Math.floor(Math.random()*(top-bottom))+bottom;
 }
+
+newReason();
