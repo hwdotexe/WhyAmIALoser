9a2f005b79b77e56f9e3b26eb577fe06650c37cd
xFyreWolfx <bukkit.firebreath15@gmail.com>
2017-09-01 00:02:31 -0600

    Update site, actually...?

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Site version 1.1
 // Created by xFyreWolfx - http://xfyrewolfx.tech/
 
-var reasons = ["You smell like green eggs and ham", "You're too school for cool", "Your grandma told you so", "Dank memes", "You wish your boyfriend was hot like I am", "You can't catch me outside", "Mayonnaise is not an instrument", "You're friends with the Nigerian Prince", "Deodorant: try it sometime", "I don't like your T-Shirt", "You're on this website", "Your height", "You've had one too many hot dogs. Or seven.", "You were grown in a lab... and you just found out", "You forgot the Alamo, didn't you?", "You're not a real boy", "You still can't find waldo", "You eat worms", "You're a comic book nerd", "Winning is illogical", "This site has deemed you one.", "You gotta get a job, hippie!", "You got your mobility back", "You're a carrot farmer", "You sunbathe on your roof. Naked. In the city.", "Science", "You've had your blinker on for the past 5 blocks", "You're waaay too old", "Victory is not yours", "We are not the champions", "Your losing streak is a world record"];
-var randomNumber, randomReason;
+var reasons = ["Your face", "You smell like green eggs and ham", "You're too school for cool", "Your grandma told you so", "Dank memes", "You wish your boyfriend was hot like I am", "You can't cash me outside", "Mayonnaise is not an instrument", "You're friends with the Nigerian Prince", "Deodorant: try it sometime", "You order seasonal drinks from Starbucks out of season", "I don't like your T-Shirt", "You're on this website", "You can't cook", "You're the reincarnation of Flappy Bird", "You're really, really boring", "You forgot the Alamo, didn't you?", "Bad theology", "You still can't find waldo", "You voted for Mickey Mouse", "You're King of the Nerds", "Winning is illogical", "This site has deemed you one", "You gotta get a job, hippie!", "You literally can't even", "You're a carrot farmer", "You sunbathe on your roof. Naked. In the city.", "You've had your blinker on for the past 5 blocks", "Victory is not yours", "We are not the champions", "Your losing streak is a world record", "Sidewalk faceplant", "You surrender", "You didn't stop in the name of love", "You kept going during Hammer Time", "If you play hide-and-seek, nobody would look for you", "You play the clarinet", "You're too weird for Weird Al", "You have a Nick Cage movie collection", "Such loser. Much wow. So shameful.", "You're still doing the Harlem Shake", "I've got a jar of dirt", "The rum is always gone"];
+var colors = ["red", "black", "#235108", "#0d3554", "#180777", "#631258", "#631218", "#7f020a", "#878e20", "#09a089", "#31a009", "#c47703"];
+var randomNumber, randomReason, randomColor;
 var i = 0;
 
 document.getElementById("again").addEventListener("click", newReason);
@@ -27,7 +27,10 @@ function newReason(){
         }
     }
 
+    randomColor = getRandom(0, colors.length);
+
     document.getElementById("reason").textContent= randomReason;
+    document.getElementById("reason").setAttribute("style", "color: "+colors[randomColor]+";");
 
     if(i<reasons.length) storePastReasons();
     i = 0;
